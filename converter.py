8f3e39277638f0a594973beca8c48c9195170645
Samrud Reddy <samrudr@gmail.com>
2023-12-12 19:46:52 +0530

    Refactored the image to text function. Has a bug where if a block is full of low confidance than the output is and aray with an aray with an empty aray

--- converter.py
+++ converter.py
@@ -1,6 +1,8 @@
 from pdf2image import convert_from_path
 import pytesseract
 from PIL import Image
+
+import numpy as np
 # target_folder = "textbooks\\chemistry\\AS&A levels"
 pytesseract.pytesseract.tesseract_cmd = r"C:\\Users\\samru\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
 
@@ -28,64 +30,98 @@ def debug_string(path):
   with open("read.txt", "w+") as f:
     f.write(out)
 
+def invert_dict_list(data):
+  converted_data = []
+
+  for i in range(len(data["level"])):
+    row = {}
+    for key in data:
+      row[key] = data[key][i]
+    
+    converted_data.append(row)
+  
+  return converted_data
+
+
+
+
+
 def stringify_image(path, treshold = 70):
   data = pytesseract.image_to_data(Image.open(path), lang="eng", output_type=pytesseract.Output.DICT)
 
-  #Remove low conf
-  to_remove = []
-  for i in range(len(data["level"])):  
-    if data["conf"][i] < treshold:
-      to_remove.append(i)
-  
-  to_remove.reverse()
-  for i in to_remove:
-    for key in data:
-      data[key].pop(i)
-
-  
   out = []
-  prev_block_no = data["block_num"][0]
-  prev_line_no = data["line_num"][0]
 
-  curr_block_list = []
-  curr_line = ""
+  cur_page = []
+  cur_block = []
+  cur_par = []
+  cur_line = []
 
-  for i in range(len(data["level"])):  
-    text = data["text"][i]
+  data = invert_dict_list(data)
 
-    curr_block_no = data["block_num"][i]
-    curr_line_no = data["line_num"][i]
+  for row in data:
 
-    if prev_block_no != curr_block_no:
-      if curr_block_list == []:
-        curr_block_list.append(curr_line)
-        
-      out.append(curr_block_list)
+    #Sets all unconfidant values to None
+    if row["conf"] < treshold:
+      row["text"] = None
+
+    match row["level"]:
+      #Word
+      case 5:
+        pass
+      #Line
+      case 4:
+        if cur_line:
+          cur_par.append(cur_line)
+          cur_line = []
+
       
-      curr_line = text
-      curr_block_list = []
+      #Paragraph
+      case 3:
+        if cur_par:
+          cur_block.append(cur_par)
+          cur_par = []
 
-      prev_block_no = curr_block_no
-      prev_line_no = curr_line_no
-      continue
-      
-    if prev_line_no != curr_line_no:
-      curr_line += " "+text
 
-      prev_line_no = curr_line_no
-      continue
+      #Block
+      case 2:
+        if cur_block:
+          cur_page.append(cur_block)
+          cur_block = []
 
-    curr_line += " " + text
 
-  
-  out = [i[0] for i in out]
+      #Page
+      case 1:
+        if cur_page:
+          out.append(cur_page)
+          cur_page = []
 
-  out = "\n\n".join(out)
+
+    cur_line.append(row["text"])
+    
+  cur_par.append(cur_line)
+  cur_block.append(cur_par)
+  cur_page.append(cur_block)
+  out.append(cur_page)
+
+  # removed none
+  out = [[[[[value for value in dim4 if value is not None] for dim4 in dim3] for dim3 in dim2] for dim2 in dim1] for dim1 in out]
+
+  #removes arrays that only contain empty arrays
+  # out = to_lazy(out)
 
   return out
 
+def to_lazy(out):
+  for page in out:
+    for block in page:
+      if block[0] == []:
+        out.remove(block)
 
 
 
-with open("read.txt", "+w") as f:
-  f.write(stringify_image("C:\\Users\\samru\\Desktop\\Code\\ASK ai\\textbooks\\chemistry\\AS&A levels\\pages\\test.jpg"))
+out = stringify_image("C:\\Users\\samru\\Desktop\\Code\\ASK ai\\textbooks\\chemistry\\AS&A levels\\pages\\test.jpg")
+
+
+print(out)
+# with open("read.txt", "+w") as f:
+#   f.write(stringify_image("C:\\Users\\samru\\Desktop\\Code\\ASK ai\\textbooks\\chemistry\\AS&A levels\\pages\\test.jpg"))
