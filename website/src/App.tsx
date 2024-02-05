61cca530455b340460a948fe03cf444bde57964e
Kushal Baranwal <imkushal01@gmail.com>
2024-02-05 17:24:02 +0530

    added basic convo functionality

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -5,16 +5,35 @@ import { IconSend2 } from "@tabler/icons-react";
 import { handleSubmit } from "./handleSubmit";
 import { useHover } from "@mantine/hooks";
 
-function App() {
+const App = () => {
     const [question, setQuestion] = useState<string>("");
+    const [subject, setSubject] = useState<string>("Chemistry"); // setting chemistry as default cuz it's the hardest
+    const [prevChatArray, setPrevChatArray] = useState<string[]>([]);
     const subjectList: string[] = ["Chemistry", "Physics", "Biology", "Comp Sci"];
     const { hovered, ref } = useHover();
 
     return (
         <div className="wrapperDiv">
             <h2 className="title">Ask AI Anything</h2>
+            <div className="chatBox">
+                {prevChatArray.map((msg: string, index: number) => {
+                    if (index % 2 == 0) {
+                        return <p key={msg + Math.random()}>You: {msg}</p>;
+                    } else {
+                        return <p key={msg + Math.random()}>Ask AI: {msg}</p>;
+                    }
+                })}
+            </div>
             <div className="inputStuff">
-                <NativeSelect size="lg" className="subjectSelect" data={subjectList} />
+                <NativeSelect
+                    value={subject}
+                    size="lg"
+                    className="subjectSelect"
+                    data={subjectList}
+                    onChange={(e) => {
+                        setSubject(e.currentTarget.value);
+                    }}
+                />
                 <TextInput
                     className="askInput"
                     placeholder="Ask AI Anything..."
@@ -23,7 +42,14 @@ function App() {
                             <IconSend2
                                 stroke={hovered ? 2 : 1}
                                 className="sendIcon"
-                                onClick={() => handleSubmit(question)}
+                                onClick={async () => {
+                                    const questionAsked = question;
+                                    setQuestion("");
+                                    setPrevChatArray((prevChatArray) => [...prevChatArray, questionAsked]);
+                                    const response = await handleSubmit(questionAsked, subject, prevChatArray);
+                                    console.log("RESPONSE IS " + response);
+                                    setPrevChatArray((prevChatArray) => [...prevChatArray, response]);
+                                }}
                             />
                         </div>
                     }
@@ -33,16 +59,20 @@ function App() {
                     onChange={(e) => {
                         setQuestion(e.currentTarget.value);
                     }}
-                    onKeyUp={(e) => {
+                    onKeyUp={async (e) => {
                         if (e.key == "Enter") {
-                            handleSubmit(question);
+                            const questionAsked = question;
                             setQuestion("");
+                            setPrevChatArray((prevChatArray) => [...prevChatArray, questionAsked]);
+                            const response = await handleSubmit(questionAsked, subject, prevChatArray);
+                            console.log("RESPONSE IS " + response);
+                            setPrevChatArray((prevChatArray) => [...prevChatArray, response]);
                         }
                     }}
                 />
             </div>
         </div>
     );
-}
+};
 
 export default App;
--- website/src/handleSubmit.ts
+++ website/src/handleSubmit.ts
@@ -1,3 +1,7 @@
-export const handleSubmit = (question: string) => {
-    console.log(question);
+export const handleSubmit = async (question: string, subject: string, prevChat: string[]): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(subject);
+        }, 300);
+    });
 };
